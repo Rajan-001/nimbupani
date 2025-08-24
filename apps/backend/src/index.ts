36542e49106710d23726caa47d4d2b8bb6309975
Rajan Palial <rajanpalial123@gmail.com>
2025-08-24 14:09:59 +0530

    changed_to_package.json

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -552,7 +552,8 @@ const { razorpay_order_id, razorpay_payment_id, razorpay_signature, orderId } = 
 });
 
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
-app.listen(8001, () => console.log("Server running on http://localhost:8001"));
+// app.listen(8001, () => console.log("Server running on http://localhost:8001"));
 
